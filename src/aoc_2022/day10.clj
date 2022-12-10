a7b812e6da26d19695043b4534b8d750bb2f4c95
ryan0583 <ryan0583@gmail.com>
2022-12-10 10:29:34 +0000

    Refactoring

--- src/aoc_2022/day10.clj
+++ src/aoc_2022/day10.clj
@@ -1,34 +1,35 @@
-(ns aoc-2022.day10 
+(ns aoc-2022.day10
   (:require [aoc-2022.utils :as utils]
             [clojure.string :refer [index-of]]))
 
-(defn process-noop [instruction-index]
+(defn process-noop! [instruction-index]
   (swap! instruction-index + 1))
 
-(defn process-command [current-instruction new-instruction]
+(defn process-command! [current-instruction new-instruction]
   (swap! current-instruction (constantly new-instruction)))
 
-(defn read-instruction 
+(defn read-instruction!
   [instruction-index current-instruction lines]
   (let [instruction     (nth lines @instruction-index)
         isnoop          (= "noop" instruction)
-        new-instruction (when (not isnoop) 
-                          (subs instruction 
+        new-instruction (when (not isnoop)
+                          (subs instruction
                                 (+ (index-of instruction " ") 1)))]
     (if isnoop
-      (process-noop instruction-index)
-      (process-command current-instruction (Integer/parseInt new-instruction))))
-  )
+      (process-noop! instruction-index)
+      (process-command! current-instruction (Integer/parseInt new-instruction)))))
 
-(defn process-instruction [instruction-index current-instruction instruction-history]
+(defn process-instruction! [instruction-index current-instruction instruction-history]
   (swap! instruction-history conj @current-instruction)
   (swap! current-instruction (constantly nil))
   (swap! instruction-index + 1))
 
-(defn perform-cycle [instruction-index current-instruction instruction-history lines]
-  (if (nil? @current-instruction) 
-    (read-instruction instruction-index current-instruction lines)
-    (process-instruction instruction-index current-instruction instruction-history)))
+(defn perform-cycle! [instruction-index current-instruction instruction-history lines]
+  (if (nil? @current-instruction)
+    (read-instruction! instruction-index current-instruction lines)
+    (process-instruction! instruction-index current-instruction instruction-history)))
+
+(defn x-register [instruction-history] (apply + instruction-history))
 
 (defn process [lines]
   (let [instruction-index   (atom 0)
@@ -37,22 +38,29 @@
         running-total       (atom [])]
     (doseq [cycle-number (range 221)]
       (when (.contains [20 60 100 140 180 220] (+ cycle-number 1))
-        ;; (println @instruction-history)
-        (swap! running-total conj (* (apply + @instruction-history) (+ cycle-number 1)))
-        ;; (println (apply + @instruction-history))
-        ;; (println (+ cycle-number 1))
-        ;; (println @running-total)
-        ;; (println)
-        )
-      (perform-cycle instruction-index current-instruction instruction-history lines) 
-      )
+        (swap! running-total conj (* (x-register @instruction-history) (+ cycle-number 1))))
+      (perform-cycle! instruction-index current-instruction instruction-history lines))
     (apply + @running-total)))
 
 (defn part1 []
   (let
    [input (utils/readlines "resources/day10/input.txt")]
-    (process input)
-    ))
+    (process input)))
+
+(defn can-see-sprite? [column-index sprite-middle] 
+  (<= (- column-index 1) sprite-middle (+ column-index 1)))
+
+(defn update-row! [crt-row column-index sprite-middle] 
+  (swap! crt-row str (if (can-see-sprite? column-index sprite-middle) "#" ".")))
+
+(defn new-line! [crt-row column-index]
+  (swap! crt-row (constantly ""))
+  (swap! column-index (constantly 0)))
+
+(defn print-row! [crt-row column-index] 
+  (when (= 40 @column-index)
+    (println @crt-row)
+    (new-line! crt-row column-index)))
 
 (defn processpt2 [lines]
   (let [instruction-index   (atom 0)
@@ -60,23 +68,11 @@
         instruction-history (atom [1])
         crt-row (atom "")
         column-index (atom 0)]
-    (doseq [cycle-number (range 240)]
-    ;;   (println cycle-number)
-      (let [sprite-middle (apply + @instruction-history)]
-        ;; (println "START CYCLE: " (+ cycle-number 1))
-
-        (swap! crt-row str (if (<= (- @column-index 1) sprite-middle (+ @column-index 1)) "#" "."))
-        (perform-cycle instruction-index current-instruction instruction-history lines)
+    (doseq [_ (range 240)]
+        (update-row! crt-row @column-index (x-register @instruction-history))
+        (perform-cycle! instruction-index current-instruction instruction-history lines)
         (swap! column-index + 1)
-        ;; (println "OLD SPRITE POSITION: " sprite-middle)
-        ;; (println "CURRENT CRT ROW: " @crt-row)
-        ;; (println "NEW SPRITE POSITION: " (apply + @instruction-history))
-        ;; (println)
-
-        (when (.contains [40 80 120 160 200 240] (+ cycle-number 1))
-          (println @crt-row)
-          (swap! crt-row (constantly ""))
-          (swap! column-index (constantly 0)))))))
+        (print-row! crt-row column-index))))
 
 (defn part2 []
   (let
