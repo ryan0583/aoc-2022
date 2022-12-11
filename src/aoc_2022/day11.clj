56b20b8c976222eba108990111c78d466b2bee58
ryan0583 <ryan0583@gmail.com>
2022-12-11 23:30:47 +0000

    Day 11 part 1

--- src/aoc_2022/day11.clj
+++ src/aoc_2022/day11.clj
@@ -1,13 +1,14 @@
 (ns aoc-2022.day11
   (:require [aoc-2022.utils :as utils]
+            [clojure.math :refer [floor]]
             [clojure.string :refer [split starts-with?]]))
 
 (defn new-monkey [monkey-lines monkey-index]
   (swap! monkey-lines conj {:items        []
                             :operation-fn {:operator "" :value ""}
-                            :test-fn      {:divisor 0 :true-monkey 0 :false-monkey 0}})
-  (swap! monkey-index + 1)
-  (println))
+                            :test-fn      {:divisor 0 :true-monkey 0 :false-monkey 0}
+                            :count-inspected 0})
+  (swap! monkey-index + 1))
 
 (defn process-items [command monkey-lines monkey-index]
   (let [items (vec (map #(Integer/parseInt %) (split (subs command 16) #",\s")))
@@ -29,9 +30,9 @@
         true-monkey (Integer/parseInt (str (last (second (nth lines (+ line-index 1))))))
         false-monkey (Integer/parseInt (str (last (second (nth lines (+ line-index 2))))))
         new-monkey-lines (assoc @monkey-lines monkey-index
-                                (assoc (nth @monkey-lines monkey-index) 
-                                       :test-fn {:divisor      divisor 
-                                                 :true-monkey  true-monkey 
+                                (assoc (nth @monkey-lines monkey-index)
+                                       :test-fn {:divisor      divisor
+                                                 :true-monkey  true-monkey
                                                  :false-monkey false-monkey}))]
     (swap! monkey-lines (constantly new-monkey-lines))))
 
@@ -49,14 +50,73 @@
     (new-monkey monkey-lines monkey-index)
     (process-line lines line monkey-lines monkey-index line-index)))
 
+(defn adjust-worry-level [item monkey]
+  (let [
+        operation-fn (get monkey :operation-fn)
+        operator (get operation-fn :operator)
+        value (get operation-fn :value)
+        value (if (= "old" value) item (Integer/parseInt value))
+        ]
+  (case operator
+    "*" (* item value)
+    "+" (+ item value))))
+
+(defn passes-test? [worry-level monkey]
+  (= (mod worry-level (get (get monkey :test-fn) :divisor)) 0))
+
+(defn item-round [item monkey-index monkey monkey-lines]
+  (let  [
+         adjusted-worry-level (adjust-worry-level item monkey)
+         adjusted-worry-level (int (floor (/ adjusted-worry-level 3)))
+         passes-test? (passes-test? adjusted-worry-level monkey)
+         test-fn (get monkey :test-fn)
+         new-monkey-index (if passes-test? (get test-fn :true-monkey)
+                              (get test-fn :false-monkey))
+         existing-new-monkey (nth @monkey-lines new-monkey-index)
+         new-monkey-lines (assoc @monkey-lines new-monkey-index
+                                (assoc existing-new-monkey
+                                       :items (conj (get existing-new-monkey :items) adjusted-worry-level)))
+         monkey-items (get monkey :items)
+         new-monkey-lines (assoc new-monkey-lines monkey-index
+                                 (assoc monkey
+                                        :items (subvec monkey-items 1)))
+        ;;  log (println new-monkey-lines)
+         ]
+    (swap! monkey-lines (constantly new-monkey-lines))
+    )
+  )
+
+(defn monkey-round [monkey-lines monkey monkey-index]
+  (let [items (get monkey :items)
+        new-monkey-lines (assoc @monkey-lines monkey-index
+                                (assoc monkey :count-inspected (+ (get monkey :count-inspected) (count items))))]
+    (swap! monkey-lines (constantly new-monkey-lines))
+    ;; (println (str "MONKEY: " monkey-index))
+    (doseq [item items]
+      (item-round item monkey-index (get @monkey-lines monkey-index) monkey-lines)
+      ;; (println)
+      )
+    ;; (println)
+    ))
+
+
+(defn do-round [monkey-lines]
+  (doseq [[index _] (map-indexed vector @monkey-lines)]
+    (monkey-round monkey-lines (get @monkey-lines index) index)))
+
 (defn process [lines]
   (let
    [monkey-lines (atom [])
     monkey-index (atom -1)]
     (doseq [[index line] lines]
       (readline lines line monkey-lines monkey-index index))
-    (println)
-    @monkey-lines))
+    (doseq [round (range 20)]
+      ;; (println round)
+      (do-round monkey-lines)
+      ;; (println)
+      )
+    ;; (println)
+    (apply * (take 2 (reverse (sort (map #(get % :count-inspected) @monkey-lines)))))))
 
 (defn part1 []
   (let
